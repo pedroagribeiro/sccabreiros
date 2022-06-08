d0c836b5fc3f2c8f3008c4096638cd21dddb7bb6
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-06-08 01:27:16 +0100

    :triangular_flag_on_post: Removes build error related to the use of <a>

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
@@ -6,15 +7,14 @@ const Navbar = () => {
       <nav className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8" aria-label="Top">
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 justify-between items-center py-6 w-full border-b border-green-500 lg:border-none">
           <div className="flex items-center">
-            <a
-              href="/"
-              className="flex flex-col lg:flex-row justify-center items-center space-x-2 text-white"
-            >
-              <p className="text-xl font-semibold">
-                Sporting Clube de Cabreiros
-              </p>
-              <p className="text-xl font-light">desde 1932</p>
-            </a>
+            <Link href="/">
+              <div className="flex flex-col lg:flex-row justify-center items-center space-x-2 text-white">
+                <p className="text-xl font-semibold">
+                  Sporting Clube de Cabreiros
+                </p>
+                <p className="text-xl font-light">desde 1932</p>
+              </div>
+            </Link>
           </div>
           <div className="flex flex-col sm:flex-row space-y-4 sm:space-x-6 sm:space-y-0 items-center">
             <a
