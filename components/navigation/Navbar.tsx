52f8c2263d1eee90bba4334ab73184345974acb7
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-06-09 17:01:36 +0100

    :recycle: Replces <a> with Next.js links

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -8,33 +8,30 @@ const Navbar = () => {
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 justify-between items-center py-6 w-full border-b border-green-500 lg:border-none">
           <div className="flex items-center">
             <Link href="/">
-              <div className="flex flex-col lg:flex-row justify-center items-center space-x-2 text-white">
+              <button className="flex flex-col lg:flex-row justify-center items-center space-x-2 text-white">
                 <p className="text-xl font-semibold">
                   Sporting Clube de Cabreiros
                 </p>
                 <p className="text-xl font-light">desde 1932</p>
-              </div>
+              </button>
             </Link>
           </div>
           <div className="flex flex-col sm:flex-row space-y-4 sm:space-x-6 sm:space-y-0 items-center">
-            <a
-              href="#"
-              className="py-2 px-8 h-10 w-40 text-sm font-medium text-center text-green-600 bg-white rounded-md border border-transparent shadow-sm hover:bg-green-50"
-            >
-              Sobre
-            </a>
-            <a
-              href="/roaster"
-              className="py-2 px-8 h-10 w-40 text-sm font-medium text-center text-green-600 bg-white rounded-md border border-transparent shadow-sm hover:bg-green-50"
-            >
-              Plantel
-            </a>
-            <a
-              href="#"
-              className="py-2 px-8 h-10 w-40 text-sm font-medium text-center text-green-600 bg-white rounded-md border border-transparent shadow-sm hover:bg-green-50"
-            >
-              Contacte-nos
-            </a>
+            <Link href="#">
+              <button className="py-2 px-8 h-10 w-40 text-sm font-medium text-center text-green-600 bg-white rounded-md border border-transparent shadow-sm hover:bg-green-50">
+                Sobre
+              </button>
+            </Link>
+            <Link href="/roaster">
+              <button className="py-2 px-8 h-10 w-40 text-sm font-medium text-center text-green-600 bg-white rounded-md border border-transparent shadow-sm hover:bg-green-50">
+                Plantel
+              </button>
+            </Link>
+            <Link href="#">
+              <button className="py-2 px-8 h-10 w-40 text-sm font-medium text-center text-green-600 bg-white rounded-md border border-transparent shadow-sm hover:bg-green-50">
+                Contacte-nos
+              </button>
+            </Link>
           </div>
         </div>
       </nav>
