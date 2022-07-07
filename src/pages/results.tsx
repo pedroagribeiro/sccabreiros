e485491a1252a08acc1123b3c861913eaa8b351e
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-07-07 13:58:44 +0100

    :label: Updates type checking on results page

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react';
-import { FaFireExtinguisher } from 'react-icons/fa';
 import Layout from '../components/layouts/Layout';
 import PastFixture from '../components/result/PastFixture';
 import { trpc } from '../utils/trpc';
@@ -15,20 +14,22 @@ const Results = () => {
     <div className="flex justify-center items-center mt-16 mb-16 text-gray-700">
       <div className="max-w-6xl flex flex-col space-y-0 items-center mx-6 md:mx-32 lg:mx-72 ">
         <div className="grid grid-cols-1 space-y-6 md:space-y-0 md:grid-cols-2 md:gap-8">
-          {fixtures?.fixtures.map((fixture) => (
-            <PastFixture
-              key={fixture.competition + fixture.round}
-              date={fixture.date}
-              location={fixture.location}
-              venue_name={fixture.venue}
-              home_team={fixture.homeTeam}
-              away_team={fixture.awayTeam}
-              home_scored_goals={fixture.homeTeamGoals}
-              away_scored_goals={fixture.awayTeamGoals}
-              competition={fixture.competition}
-              round={fixture.round}
-            />
-          ))}
+          {fixtures?.fixtures
+            .filter((f) => f.homeTeamGoals && f.awayTeamGoals && f.round)
+            .map((fixture) => (
+              <PastFixture
+                key={fixture.competition + fixture.round}
+                date={fixture.date}
+                location={fixture.location}
+                venue_name={fixture.venue}
+                home_team={fixture.homeTeam}
+                away_team={fixture.awayTeam}
+                home_scored_goals={fixture.homeTeamGoals!}
+                away_scored_goals={fixture.awayTeamGoals!}
+                competition={fixture.competition}
+                round={fixture.round!}
+              />
+            ))}
         </div>
       </div>
     </div>
