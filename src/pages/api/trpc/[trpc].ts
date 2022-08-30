622d6c5058e58573f2104fbf151341f3ec22fe65
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-08-30 18:01:45 +0100

    :arrow_down: Undo CORS fix

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,15 +1,12 @@
 import { appRouter, AppRouter } from '../../../server/routers';
 import { inferProcedureOutput } from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
-import { withCors } from '../../../server/utils/cors';
 
 // export API handler
-export default withCors(
-  trpcNext.createNextApiHandler({
-    router: appRouter,
-    createContext: () => null,
-  }),
-);
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
 
 export type inferQueryResponse<
   TRouteKey extends keyof AppRouter['_def']['queries'],
